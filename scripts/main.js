d015185a0a6c9eb5df7b7685e9b2982700536d85
Olesya Barmina <thelesenok@gmail.com>
2019-01-04 13:17:05 +0300

    interactive initial commit

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
