0ed2afb02d66aeeb34ea0030a706fa4ca53c34be
Olesya Barmina <thelesenok@gmail.com>
2019-01-08 21:23:04 +0300

    feature-popup-photo added

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
-const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
-const HIDDEN_DETAIL_CLASS = 'hidden-detail';
-const TINY_EFFECT_CLASS = 'tiny-image';
-const ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -35,8 +31,7 @@ function addThumbClickHadler(thumb) {
     thumb.addEventListener('click', function(event) {
         event.preventDefault();
         setDetailsFromThumb(thumb);
-        showDetails();
-    }) 
+    })
 }
 
 function getThumbnailsArray() {
@@ -46,47 +41,12 @@ function getThumbnailsArray() {
     return thumbnailArray;
 }
 
-function hideDetails() {
-    'use strict';
-    document.body.classList.add(HIDDEN_DETAIL_CLASS);
-}
-
-function showDetails() {
-    'use strict';
-    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
-    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
-    frame.classList.add(TINY_EFFECT_CLASS);
-    setTimeout(function() {
-        frame.classList.remove(TINY_EFFECT_CLASS);
-    }, 50);
-    
-}
-
-function addKeyPressedHadler() {
-    'use strict';
-    document.body.addEventListener('keyup', function(event) {
-        event.preventDefault();
-        if (event.keyCode === 27) {
-            hideDetails();
-        }
-    })
-}
-
 function initilizeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickHadler);
-    // setThumbnails();
-    addKeyPressedHadler();
-}
-
-function setThumbnails() {
-    'use strict';
-    var thumbnails = getThumbnailsArray();
-    thumbnails.forEach(function (thumbnail) {
-        var randomThumbIndex =1 + Math.round(Math.random(2) * 2);
-        thumbnail.setAttribute('data-image-url', 'img/corgi-' + randomThumbIndex +'.jpg');
-    })
 }
  
-initilizeEvents();
+$(document).ready(function(){
+    initilizeEvents();
+});
